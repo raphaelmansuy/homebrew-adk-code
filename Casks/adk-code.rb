d5881e5621e939f118c6da5745da5ca45b97c1aa
Raphael Mansuy <raphael.mansuy@gmail.com>
2025-11-14 14:15:12 +0800

    chore: update to v0.1.1 with correct SHA256 hashes

--- Casks/adk-code.rb
+++ Casks/adk-code.rb
@@ -1,12 +1,13 @@
 cask "adk-code" do
-  version "0.0.1"
-  sha256 :no_check
+  version "0.1.1"
   
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/raphaelmansuy/adk-code/releases/download/v#{version}/adk-code-v#{version}-darwin-arm64"
+      sha256 "b8639d0e6b7ea9e0d4f99ba768c32741f928b6730d990e991e585ba614287e6f"
     elsif Hardware::CPU.intel?
       url "https://github.com/raphaelmansuy/adk-code/releases/download/v#{version}/adk-code-v#{version}-darwin-amd64"
+      sha256 "69958d07a90d0422ef3cb07e0096b6e7858ae075e2b3216cddc924968ea766b7"
     end
   end
 
