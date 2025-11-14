feb9b6318942a3b711882c00a10080cc232b7789
Raphael Mansuy <raphael.mansuy@gmail.com>
2025-11-14 17:05:51 +0800

    chore(cask): update adk-code to use latest version and remove specific sha256

--- Casks/adk-code.rb
+++ Casks/adk-code.rb
@@ -1,17 +1,20 @@
 cask "adk-code" do
   arch arm: "arm64", intel: "amd64"
   
-      version "0.2.1"
-      sha256 arm:   "ff75da3afba2030751a1dc8202956db1530827545673786ff2ffc48fa1f5faad",
-        intel: "f2bb99340062d344385b5bd71569ea0c7a1957b251d32f83ab5d6c1a7537de89"
+  version :latest
+  sha256 :no_check
 
-  url "https://github.com/raphaelmansuy/adk-code/releases/download/v#{version}/adk-code-v#{version}-darwin-#{arch}"
+  url do |version|
+    "https://github.com/raphaelmansuy/adk-code/releases/download/v#{version}/adk-code-v#{version}-darwin-#{arch}"
+  end
+  
   name "adk-code"
   desc "Command-line tool for adk-code"
   homepage "https://github.com/raphaelmansuy/adk-code"
 
   livecheck do
-    url :url
+    url "https://github.com/raphaelmansuy/adk-code/releases.atom"
+    regex(%r{/releases/tag/v?(\d+(?:\.\d+)*)}i)
     strategy :github_latest
   end
 
@@ -24,8 +27,6 @@ cask "adk-code" do
                    args: ["-d", "com.apple.quarantine", "#{HOMEBREW_PREFIX}/bin/adk-code"],
                    sudo: false
   end
-
-  # v0.2.1 includes CGO support for SQLite (PR #9)
 
   zap trash: [
     "~/.adk-code",
