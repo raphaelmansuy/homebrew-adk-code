99808d6164b2eccbf284a20e971aab3d92a13325
Raphael Mansuy <raphael.mansuy@gmail.com>
2025-11-14 14:13:59 +0800

    fix: use sha256 :no_check for cask until release binaries are available

--- Casks/adk-code.rb
+++ Casks/adk-code.rb
@@ -1,13 +1,12 @@
 cask "adk-code" do
   version "0.0.1"
+  sha256 :no_check
   
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/raphaelmansuy/adk-code/releases/download/v#{version}/adk-code-v#{version}-darwin-arm64"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
     elsif Hardware::CPU.intel?
       url "https://github.com/raphaelmansuy/adk-code/releases/download/v#{version}/adk-code-v#{version}-darwin-amd64"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
     end
   end
 
